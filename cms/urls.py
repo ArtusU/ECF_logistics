89daf288743344f9fe270f04ab4244cbbd6ddecd
ArtusU <artusrock@hotmail.com>
2020-11-16 09:48:35 +0000

    Referrer dashboard

--- cms/urls.py
+++ cms/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.home),
     path('delivery-day/', views.delivery),
     path('run/', views.run),
+    path('order-details/<str:pk>', views.orderDetails),
+    path('referrer-dashboard/<str:pk>', views.referrerDashboard),
 ]
--- cms/views.py
+++ cms/views.py
@@ -1,11 +1,32 @@
+from cms.models import Order
 from django.http import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Order, Recipient, Referrer
 
 
 
 def home(request):
-    return render(request, 'cms/admin_pan.html')
+    orders = Order.objects.all()
+
+    new_orders = Order.objects.exclude(delivery_day='*').count()
+    monday_orders = Order.objects.filter(delivery_day='Monday').count()
+    tuesday_orders = Order.objects.filter(delivery_day='Tuesday').count()
+    wednesday_orders = Order.objects.filter(delivery_day='Wednesday').count()
+    thursday_orders = Order.objects.filter(delivery_day='Thursday').count()
+    friday_orders = Order.objects.filter(delivery_day='Friday').count()
+
+    
+    context = {
+        'new_orders': new_orders,
+        'orders': orders,
+        'monday_orders': monday_orders,
+        'tuesday_orders': tuesday_orders,
+        'wednesday_orders': wednesday_orders,
+        'thursday_orders': thursday_orders,
+        'friday_orders': friday_orders,
+    }
+    return render(request, 'cms/admin_pan.html', context)
 
 
 def delivery(request):
@@ -14,3 +35,22 @@ def delivery(request):
 
 def run(request):
     return render(request, 'cms/run.html')
+
+def orderDetails(request, pk):
+    order_details = Order.objects.get(id=pk)
+    context = {
+        'order_details': order_details
+    }
+    return render(request, 'cms/order_details.html', context)
+
+
+def referrerDashboard(request, pk):
+    referrer = Referrer.objects.get(id=pk)
+    recipients = Recipient.objects.all()
+
+    context = {
+        'referrer': referrer,
+        'recipients': recipients
+    }
+
+    return render(request, 'cms/referrer_dashboard.html', context)
