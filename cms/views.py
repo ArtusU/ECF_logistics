03e6e7e3f55994537feeec541b828cc073b0d90b
ArtusU <artusrock@hotmail.com>
2020-11-18 14:12:53 +0000

    user login

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,4 @@
+from django.forms.widgets import PasswordInput
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
@@ -22,10 +23,23 @@ def registerPage(request):
             return redirect("/")
     
     context = {'form': form}
-    return render(request, 'cms/register.html', context)
+    return render(request, 'cms/login.html', context)
 
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("/")
+        else:
+            messages.info(request, 'Username or Password is incorrect')
+
     context = {}
     return render(request, 'cms/login.html', context)
 
